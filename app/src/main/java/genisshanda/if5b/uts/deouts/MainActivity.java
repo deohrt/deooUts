782e15b6f11461d7e88176ebda91b7615440d683
deohrt <deohartanto@mhs.mdp.ac.id>
2022-11-02 14:46:24 +0700

    Initial commit

---
+++ app/src/main/java/genisshanda/if5b/uts/deouts/MainActivity.java
@@ -0,0 +1,14 @@
+package genisshanda.if5b.uts.deouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
